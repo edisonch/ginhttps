e41a9e006448dbdbf2bbb9e9d992e4b7f13c68ec
anastasia p <anas@scandiumsc.com>
2022-02-04 19:33:22 +0700

    change header

--- ui/config-overrides.js
+++ ui/config-overrides.js
@@ -1,6 +1,15 @@
 const { paths } = require('react-app-rewired');
-// require normalized overrides
-const overrides = require('react-app-rewired/config-overrides');
+// load environment variables from .env files
+// before overrides scripts are read
+require(paths.scriptVersion + '/config/env');
+const override = require('react-app-rewired/config-overrides');
 const config = require(paths.scriptVersion + '/config/webpack.config.dev');
 
-module.exports = overrides.webpack(config, process.env.NODE_ENV);
+if (override.devserver) {
+    console.log(
+        'Warning: `devserver` has been deprecated. Please use `devServer` instead as ' +
+        '`devserver` will not be used in the next major release.'
+    )
+}
+
+module.exports = override.webpack(config, process.env.NODE_ENV);
